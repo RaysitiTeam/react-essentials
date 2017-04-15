14ccc999ddd57f9c3996831be00d186d93cfdad9
avj2352 <pramod.jingade@philips.com>
2017-04-15 11:50:49 +0530

    Made Changes to the lesson2 JS file

--- src/app/components/lesson2.js
+++ src/app/components/lesson2.js
@@ -1,40 +1,77 @@
 module.exports = function(){
  //Main Game Component which will hold all the Game inner components
  class Game extends React.Component{
+     state = {
+        selectedNumbers: [2,4]
+     };
      render(){
       return (
-          <div>
+          <div className="row">
               <Stars/>
               <Buttons/>
-              <Answers/>
+              <Answers selectedNumbers = {this.state.selectedNumbers}/>
+              <Numbers/>
           </div>
       );
      }//end:render
  }//end:Game class-component
 
  const Stars = (props)=>{
+     //Create a dynamic number of stars
+     const numberOfStars = 1 + Math.floor(Math.random()*9);
+     let stars = [];
+     for(let i =0; i<numberOfStars;i++){
+         stars.push(<i key={i} className="fa fa-star"></i>)
+     }
   return (
-      <div>Stars....</div>
+      <div className="col-lg-5">
+          {stars}
+      </div>
   );
  };//end:Stars function-component
 
  const Buttons  = (props)=>{
   return (
-      <div>Buttons...</div>
+      <div className="col-lg-2">
+          <button>=</button>
+      </div>
   );
  };//end:Buttons function-component
 
  const Answers = (props)=>{
    return(
-       <div>Answers...</div>
+       <div className="col-lg-5">
+            <div>
+                {props.selectedNumbers.map((number,i)=><span key={i}>{number}</span>)}
+            </div>
+       </div>
    );
  };//end:Answers function-component
+
+ const Numbers = (props)=>{
+    return(
+        <div className="col-lg-12 numbers-area text-center">
+            <div>
+                {
+                    Numbers.list.map((number,i)=>
+                    <span key={i}>{number}</span>
+                    )}
+            </div>
+        </div>
+    );
+ };//end:Numbers function-component
+
+ //Numbers can also be referenced as objects
+ Numbers.list = [1,2,3,4,5,6,7,8,9];
+ //If lodash is imported
+//  Numbers.list = _.range(1,10);
 
  class App extends React.Component{
     render(){
         return(
-            <div className="col-lg-12 mainApp">
+            <div className="container-fluid mainApp">
                 <h2>Play Nine...</h2>
+                <hr/>
                 <Game/>
             </div>
         );
